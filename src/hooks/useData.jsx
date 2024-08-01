8d78a1d39456ca8d305d0aa843a60f117368a168
Javier <javier.jc942@gmail.com>
2024-08-01 17:34:12 -0400

    funciones al navbar

---
+++ src/hooks/useData.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+export const useData = () => {
+    const [city, setCity] = useState('')
+    const [icon, setIcon] = useState(null)
+    const [temperature, setTemperature] = useState(null)
+    const [description, setDescription] = useState('')
+    const [windSpeed, setWindSpeed] = useState([])
+    const [windDegree, setWindDegree] = useState([])
+    const [humidity, setHumidity] = useState([])
+    const [visibility, setVisibility] = useState([])
+    const [air, setAir] = useState([])
+    const fetchWeatherData = async (url) => {
+        const rs = await axios.get(url)
+        setCity(rs.data.name)
+        setIcon(rs.data.weather[0].icon)
+        setTemperature(Math.round(rs.data.main.temp))
+        setDescription(rs.data.weather[0].description)
+        setWindSpeed(rs.data.wind.speed)
+        setWindDegree(rs.data.wind.deg)
+        setHumidity(rs.data.main.humidity)
+        setVisibility(rs.data.visibility)
+        setAir(rs.data.main.pressure)
+    }
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                // Función de éxito
+                const latitud = position.coords.latitude;
+                const longitud = position.coords.longitude;
+                const apiKey = 'bd9dc44134d81a9ff53c6b13a921e023';
+                const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apiKey}&units=metric&lang=es`;
+                fetchWeatherData(url);
+            },
+                (error) => {
+                    // Función de error
+                    console.error("Error al obtener la ubicación: ", error);
+                    alert("No se pudo obtener la ubicación.");
+                },
+                {
+                    enableHighAccuracy: true, // Solicita una alta precisión si es posible
+                    timeout: 5000, // Tiempo máximo en milisegundos para la obtención de la ubicación
+                    maximumAge: 0 // No utiliza una ubicación en caché
+                }
+            );
+        } else { alert("La geolocalización no está soportada por este navegador.") }
+    }
+    useEffect(() => { getLocation() }, [])
+    return { city, icon, temperature, description, windSpeed, windDegree, humidity, visibility, air }
+}
---
+++ src/hooks/useIcon.jsx
@@ -0,0 +1,38 @@
+import { useData } from './useData';
+import Clear from '../assets/Clear.png';
+import LightCloud from '../assets/LightCloud.png';
+import HeavyCloud from '../assets/HeavyCloud.png';
+import HeavyRain from '../assets/HeavyRain.png';
+import LightRain from '../assets/LightRain.png';
+import Shower from '../assets/Shower.png';
+import Thunderstorm from '../assets/Thunderstorm.png';
+import Snow from '../assets/Snow.png';
+import Hail from '../assets/Hail.png';
+export function useIcon() {
+    const { icon } = useData()
+    const today = new Date();
+    const options = { weekday: 'short', month: 'short', day: 'numeric' };
+    const formatDate = today.toLocaleString('en-EN', options);
+    const iconMap = {
+        '01d': Clear,
+        '01n': Clear,
+        '02d': LightCloud,
+        '02n': LightCloud,
+        '03d': HeavyCloud,
+        '03n': HeavyCloud,
+        '04d': HeavyRain,
+        '04n': HeavyRain,
+        '09d': LightRain,
+        '09n': LightRain,
+        '10d': Shower,
+        '10n': Shower,
+        '11d': Thunderstorm,
+        '11n': Thunderstorm,
+        '13d': Snow,
+        '13n': Snow,
+        '50d': Hail,
+        '50n': Hail,
+    };
+    const iconUrl = iconMap[icon] || Clear;
+    return { iconUrl, formatDate };
+}
